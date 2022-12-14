d6592f5d203ab1448d2f1051f049b5eeb1f94420
kidam77 <kirwaadam77@gmail.com>
2022-12-14 14:06:17 -0800

    last_digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,8 +8,18 @@
  */
 
 int print_last_digit(int n)
-{
-	int last_digit = n % 10;
 
-	return (last_digit);
+{
+	int l = n % 10;
+
+	if (l < 0)
+	{
+		_putchar(-l + 48);
+		return (-l);
+	}
+	else
+	{
+		_putchar(l + 48);
+		return (last_digit);
+	}
 }
