b8abe0a9d5e8a8b85604133f30e94a7eb8ce8413
kidam77 <kirwaadam77@gmail.com>
2022-12-14 14:44:15 -0800

    9_times_table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,50 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: Always 0 (Success)
+ */
+
+void times_table(void)
+{
+	int x, y, z;
+
+	for (x = 0; a <= 9; x++)
+	{
+		for (y = 0; y <= 9; y++)
+		{
+			z = x * y;
+			if ((z / 10) == 0)
+			{
+				if (y == 0)
+				{
+					_putchar('0');
+				}
+
+				if (y != 0)
+				{
+					_putchar(' ');
+					_putchar((z % 10) + '0');
+				}
+
+				if (y < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+			else
+			{
+				_putchar((z / 10) + '0');
+				_putchar((z % 10) + '0');
+
+				if (y < 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+			}
+		}
+	}
+}
