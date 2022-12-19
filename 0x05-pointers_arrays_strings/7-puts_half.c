66d258363a58ea01621873f294e485ce7c9818ae
kidam77 <kirwaadam77@gmail.com>
2022-12-19 14:44:03 -0800

    put half

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,12 +17,13 @@ void puts_half(char *str)
 		count++;
 	}
 
-	if (count % 2 == 0)
+	if (count % 2 == 1)
 		i = count / 2;
 	else
-		i = (count + 1) / 2;
+		i = (count - 1) / 2;
 	for (i++; i < count; i++)
+	{
 		_putchar(str[i]);
-
+	}
 	_putchar('\n');
 }
