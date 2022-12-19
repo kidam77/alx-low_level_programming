469479aa212a8f616c88f8e39fdf4e97a5c5ccff
kidam77 <kirwaadam77@gmail.com>
2022-12-19 12:44:49 -0800

    reset to 98

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H_INLUDED
+#define MAIN_H_INCLUDED
+
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+int _putchar(char c);
+void _puts(char *str);
+int _strlen(char *s);
+void print_rev(char *s);
+void puts2(char *str);
+void rev_string(char *s);
+void puts_half(char *str);
+void print_arrray(int *a, int *b);
+char *_strcpy(char *dest, char *src);
+
+#endif
