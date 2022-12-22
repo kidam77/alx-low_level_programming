e462ff4e9329a65a33e7b1057a2e2cb1d86e46a9
kidam77 <kirwaadam77@gmail.com>
2022-12-22 11:46:50 -0800

    task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,24 @@
+#include "main"
+
+/**
+ * string_toupper - changes all lower case letters in string to uppercase
+ * @word: pointer to string
+ * Return: 0
+ */
+
+char *string_toupper(char *word)
+{
+	int length;
+
+	length = 0;
+
+	while (word[length] != '\0')
+	{
+		if (word[length] >= 97 && word[length] <= 122)
+		{
+			word[length] = word[length] - 32;
+		}
+		length++;
+	}
+	return (word);
+}
